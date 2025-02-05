a94447b2b2f0fcac0225470a9ebf275d0f3b484a
Ty Janik <tyjanik44@gmail.com>
2025-02-05 16:32:27 -0500

    Map auto pans to uploaded location and places pin

--- emergency-app-frontend/src/App.jsx
+++ emergency-app-frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Map from "./components/Map.jsx"
 
 function App() {
     const [selectedFile, setSelectedFile] = useState(null)
-    const [lat, setLat] = useState(null)
+    const [uploadedData, setUploadedData] = useState(null)
+    const [location, setLocation] = useState(null)
+    const [locationList, setLocationList] = useState([])
 
     const onFileChange = (event) => {
         setSelectedFile(event.target.files[0])
@@ -15,28 +17,44 @@ function App() {
         return JSON.parse(fileText)
     }
 
-    const parseJsonFile = async(file) => {
+    const parseJsonFile = async (file) => {
         const incidentData = await readJsonFile(file)
-        const address = incidentData.address
-        setLat(address.latitude)
+        setUploadedData(incidentData)
     }
 
-    // Parse file if one is uploaded
-    if (selectedFile) {
-        parseJsonFile(selectedFile)
-    }
+    useEffect(() => {
+        if (location) {
+            setLocationList([location])
+        }
+    }, [location]);
 
+    useEffect(() => {
+        if (uploadedData?.address?.latitude && uploadedData?.address?.longitude) {
+            const address = uploadedData.address
+            setLocation({
+                latitude: address.latitude,
+                longitude: address.longitude
+            })
 
-  return (
-    <>
-        <h2>Upload your file below:</h2>
-        <div>
-            <input type={"file"} onChange={onFileChange} />
-            {lat}
-        </div>
-        <Map/>
-    </>
-  )
+        }
+    }, [uploadedData]);
+
+    useEffect(() => {
+        // Parse file if one is uploaded
+        if (selectedFile) {
+            parseJsonFile(selectedFile)
+        }
+    }, [selectedFile]);
+
+    return (
+        <>
+            <h2>Upload your file below:</h2>
+            <div>
+                <input type={"file"} onChange={onFileChange}/>
+            </div>
+            <Map locationList={locationList} />
+        </>
+    )
 }
 
 export default App
--- emergency-app-frontend/src/components/Map.jsx
+++ emergency-app-frontend/src/components/Map.jsx
@@ -1,20 +1,51 @@
-import React from "react"
-import {MapContainer, TileLayer, useMap, Marker, Popup} from 'react-leaflet'
+import React, { useEffect, useState } from "react"
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import PropTypes from 'prop-types'
 
-export default function Map() {
-    const startingLoc = [41.8781, -87.6298];
+export default function Map(props) {
+    const initialStartingLoc = [41.8781, -87.6298]
+    const [startingLoc, setStartingLoc] = useState(initialStartingLoc)
+
+    const locationList = props.locationList
+
+    useEffect(() => {
+        if (locationList && locationList[0]?.latitude && locationList[0]?.longitude) {
+            setStartingLoc([locationList[0].latitude, locationList[0].longitude])
+        }
+    }, [locationList]);
+
+    const Recenter = ({lat, long}) => {
+        const map = useMap()
+        useEffect(() => {
+            map.setView([lat, long])
+        }, [lat, long]);
+    }
+
+    const createMarkers = (locationList) => {
+        if (locationList) {
+            return locationList.map((loc, idx) => <Marker key={idx} position={[loc.latitude, loc.longitude]}/>)
+        }
+    }
 
     return (
-    <MapContainer center={startingLoc} zoom={8} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={startingLoc}>
-        <Popup>
-          This is a popup
-        </Popup>
-      </Marker>
-    </MapContainer>
-  )
+        <>
+            <MapContainer center={startingLoc} zoom={8} scrollWheelZoom={true}>
+                <Recenter lat={startingLoc[0]} long={startingLoc[1]}/>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {/*<Marker position={startingLoc}>*/}
+                {/*    <Popup>*/}
+                {/*        This is a popup*/}
+                {/*    </Popup>*/}
+                {/*</Marker>*/}
+                {createMarkers(props.locationList)}
+            </MapContainer>
+        </>
+    )
+}
+
+Map.propTypes = {
+    locationList: PropTypes.array
 }
