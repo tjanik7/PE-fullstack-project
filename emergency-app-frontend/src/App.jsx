571cb8ff71dc0f7638e77b3dd0fdd0b5e5588f68
Ty Janik <tyjanik44@gmail.com>
2025-02-05 13:32:11 -0500

    Able to parse and present latitude

--- emergency-app-frontend/src/App.jsx
+++ emergency-app-frontend/src/App.jsx
@@ -4,16 +4,35 @@ import Map from "./components/Map.jsx"
 
 function App() {
     const [selectedFile, setSelectedFile] = useState(null)
+    const [lat, setLat] = useState(null)
 
     const onFileChange = (event) => {
         setSelectedFile(event.target.files[0])
     }
+
+    const readJsonFile = async (file) => {
+        const fileText = await file.text()
+        return JSON.parse(fileText)
+    }
+
+    const parseJsonFile = async(file) => {
+        const incidentData = await readJsonFile(file)
+        const address = incidentData.address
+        setLat(address.latitude)
+    }
+
+    // Parse file if one is uploaded
+    if (selectedFile) {
+        parseJsonFile(selectedFile)
+    }
+
 
   return (
     <>
         <h2>Upload your file below:</h2>
         <div>
             <input type={"file"} onChange={onFileChange} />
+            {lat}
         </div>
         <Map/>
     </>
